35c805d82f10de2ecbb7954cff321a2c73b9bd93
Ali <alisy5403@gmail.com>
2026-09-20 01:21:51 +0300

    Auto migrate and create admin

--- meras_backend/wsgi.py
+++ meras_backend/wsgi.py
@@ -1,16 +1,21 @@
-"""
-WSGI config for meras_backend project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meras_backend.settings')
 
 application = get_wsgi_application()
+
+# --- الكود التالي سيبني قاعدة البيانات وحساب الإدارة تلقائياً ---
+try:
+    from django.core.management import call_command
+    from django.contrib.auth import get_user_model
+    
+    # بناء الجداول
+    call_command('migrate', interactive=False)
+    
+    # إنشاء حساب أدمن إذا لم يكن موجوداً
+    User = get_user_model()
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@admin.com', 'admin1234')
+except Exception:
+    pass
